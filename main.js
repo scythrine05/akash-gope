8d724103d8d1d798d8515b9ee9de88bfc961b528
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2024-03-15 06:28:47 +0530

    Designed Template and Animations

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-setupCounter(document.querySelector('#counter'))
+  gsap.to(".wb", {
+    transformOrigin: "center center",
+    scale: 75,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".page_1-component",
+      start: "top top",
+      end: "+=50%",
+      pin: true,
+      scrub: 1,
+    },
+  });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+const additionalX = { val: 0 };
+let offset = 0;
+let originalImages = gsap.utils.toArray(".image");
+const container = document.querySelector(".gallery");
+const sliderWidth = container.offsetWidth;
+
+// DUPLICATE IMAGES FOR LOOP
+originalImages.forEach((image) => {
+  var clone = image.cloneNode(true);
+  container.appendChild(clone);
+});
+
+let images = gsap.utils.toArray(".image");
+
+// SET ANIMATION
+images.forEach((item) => {
+  gsap.to(item, {
+    x: "-=" + Number(sliderWidth / 2),
+    duration: 20,
+    repeat: -1,
+    ease: "none",
+    modifiers: {
+      x: gsap.utils.unitize((x) => {
+        offset += additionalX.val;
+        x = (parseFloat(x) + offset) % -Number(sliderWidth * 0.5);
+        return x;
+      })
+    }
+  });
+});
+
+
+var textboxes = document.querySelectorAll(".input-text");
+
+// Iterate over each textbox
+textboxes.forEach(function (textbox) {
+  // Add an event listener for the 'input' event
+  textbox.addEventListener("input", function () {
+    // Check if the textbox has some value
+    if (this.value.trim() !== "") {
+      // If it has a value, add the 'not-empty' class
+      this.classList.add("not-empty");
+    } else {
+      // If it's empty, remove the 'not-empty' class
+      this.classList.remove("not-empty");
+    }
+  });
+});
